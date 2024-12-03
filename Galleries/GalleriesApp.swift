6cf31e93ef38b36b91fa407aef175d58fac40e36
Jelle Goemaere <Jelle.Goemaere@iMacQ.local>
2024-12-03 12:34:28 +0100

    Version 1.0

---
+++ Galleries/GalleriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleriesApp.swift
+//  Galleries
+//
+//  Created by Jelle Goemaere on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GalleriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GalleriesTests/GalleriesTests.swift
@@ -0,0 +1,16 @@
+//
+//  GalleriesTests.swift
+//  GalleriesTests
+//
+//  Created by Jelle Goemaere on 03/12/2024.
+//
+
+import Testing
+
+struct GalleriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
